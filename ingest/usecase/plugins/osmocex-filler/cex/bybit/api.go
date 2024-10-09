768cee55f6ae3ecf39b17c1f94b3290a2b770a19
Ruslan <rakhtariev@icloud.com>
2024-10-09 02:10:32 +0000

    websocket callback imm return

--- ingest/usecase/plugins/osmocex-filler/cex/bybit/api.go
+++ ingest/usecase/plugins/osmocex-filler/cex/bybit/api.go
@@ -1,10 +1,9 @@
 package bybit
 
 import (
-	"time"
-
 	wsbybit "github.com/hirokisan/bybit/v2"
 	"github.com/osmosis-labs/sqs/ingest/usecase/plugins/osmocex-filler/cex"
+	"go.uber.org/zap"
 )
 
 // subscribeOrderbook: https://bybit-exchange.github.io/docs/v5/websocket/public/orderbook
@@ -16,32 +15,29 @@ func (be *BybitExchange) subscribeOrderbook(pair cex.Pair, depth int) error {
 		},
 		// These callbacks are ran sequentially. Introduced concurrency is needed for performance.
 		func(resp wsbybit.V5WebsocketPublicOrderBookResponse) error {
-			if !be.newBlockSignal.Load() {
-				return nil
-			}
-			defer be.newBlockWg.Done()
-
-			ec := make(chan error)
-			defer close(ec)
-
 			go func() {
+				if !be.newBlockSignal.Load() {
+					return
+				}
+
+				defer be.newBlockWg.Done()
+
 				// get orderbooks from CEX and DEX
 				cexOrderbook := parseBybitOrderbook(resp.Data)
 				osmoOrderbook, err := be.getOrderbookForPair(pair)
 				if err != nil {
-					ec <- err
+					be.logger.Error("failed to get orderbook for pair", zap.String("pair", pair.String()), zap.Error(err))
+					return
 				}
 
-				ec <- be.matchOrderbooks(cexOrderbook, osmoOrderbook)
+				err = be.matchOrderbooks(cexOrderbook, osmoOrderbook)
+				if err != nil {
+					be.logger.Error("failed to match orderbooks", zap.String("pair", pair.String()), zap.Error(err))
+					return
+				}
 			}()
 
-			select {
-			case err := <-ec:
-				return err
-			case <-time.After(5 * time.Second):
-				be.logger.Info("timeout waiting for ProcessOrderbookDataAsync")
-				return nil
-			}
+			return nil // immediately return to not block the websocket
 		},
 	)
 
